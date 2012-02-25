b4e160f9e8fc5db824b74d0bcda6f6f857f422f2
Abhijit Deshmukh <asdeshmukh@gmail.com>
2012-02-25 01:09:22 -0800

    remove server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-// Basic http server, doesnt do much
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
-
